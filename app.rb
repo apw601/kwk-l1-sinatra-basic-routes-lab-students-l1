7f24728c40651d4d23b1fbf3eb63e29deebf2367
apw601 <apw601@gmail.com>
2018-06-12 21:31:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello World!"
+  end
+  get '/name' do
+    "My name is Alex"
+  end
+  get '/hometown' do
+    "My hometown is Detroit"
+  end
+  get '/favorite-song' do
+    "My favorite song is I'm upset"
+  end
 end
