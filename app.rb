c1168cff227ba284d2a0fb8f525e57807c117ce0
apw601 <apw601@gmail.com>
2018-06-12 22:41:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ class App < Sinatra::Base
   get '/' do
     "Hello World!"
   end
+<<<<<<< HEAD
   get '/name' do
     "My name is Alex"
   end
@@ -13,4 +14,15 @@ class App < Sinatra::Base
   get '/favorite-song' do
     "My favorite song is I'm upset"
   end
+=======
+get '/name' do
+  "My name is Alex"
+end
+get '/hometown'
+  "My hometown is Detroit"
+end
+get '/favorite_song'
+  "I'm upset"
+end
+>>>>>>> 1902a27403e2053a6d9ed1545dfe3a877a10331e
 end
